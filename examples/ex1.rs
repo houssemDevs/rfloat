0a39375753d60b2e528dbaf96727629efe5fede5
GHIAT Houssem <ghiat.houssem@gmail.com>
2016-05-29 09:51:47 +0200

    .

--- examples/ex1.rs
+++ examples/ex1.rs
@@ -3,20 +3,24 @@ extern crate frounding;
 extern crate num_traits;
 
 
-use num_traits::Float;
-use rfloat::rf64;
+use num_traits::{Float,Num};
+use rfloat::{rf64,rf32};
 use frounding::RoundingState;
 
-fn main() {
-    let mut rstate = RoundingState::new();
+fn main() {    
     let mut x = rf64(1.0);
     let y = rf64(3.0);
-    rstate.upward();
-    println!("{:.100}", x / y);
-    rstate.downward();
-    println!("{:.100}", x / y);
-    println!("{}", x == y);
-    println!("{}", x.sin());
+    {
+    	let mut rstate = RoundingState::new();
+    	rstate.upward();
+    	println!("{:.17}", x / y);
+    	rstate.downward();
+    	println!("{:.17}", x / y);
+   	 	println!("{}", x == y);
+    	println!("{}", x.sin());
+    }
+    let tt = rf64::from_str_radix("23.4E2", 10);
+    println!("{}", tt.unwrap());
     x += 15.0;
     println!("{}", x);
 }
