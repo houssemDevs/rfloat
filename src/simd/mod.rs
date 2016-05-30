9b9cc9ae9b60ad60d34319dafbf88ad3389963f9
GHIAT Houssem <ghiat.houssem@gmail.com>
2016-05-30 17:10:22 +0200

    add sse2 trait for arithmetic operation.

--- src/simd/mod.rs
+++ src/simd/mod.rs
@@ -9,3 +9,14 @@ pub trait SseArth {
 	fn maxps(self, rhs:Self) -> Self;
 	fn minps(self, rhs:Self) -> Self;
 }
+
+
+pub trait Sse2Arth {
+	fn addpd(self, rhs:Self) -> Self;
+	fn subpd(self, rhs:Self) -> Self;
+	fn mulpd(self, rhs:Self) -> Self;
+	fn divpd(self, rhs:Self) -> Self;
+	fn sqrtpd(self) -> Self;
+	fn maxpd(self, rhs:Self) -> Self;
+	fn minpd(self, rhs:Self) -> Self;
+}
