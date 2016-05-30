3483893915848d3c1cef68c9c52530e2efc1b632
GHIAT Houssem <ghiat.houssem@gmail.com>
2016-05-30 17:11:51 +0200

    some modifications.

--- examples/ex1.rs
+++ examples/ex1.rs
@@ -1,43 +1,20 @@
+#[macro_use]
 extern crate rfloat;
 extern crate frounding;
 extern crate num_traits;
 
-
-use num_traits::{Float,Num};
 use rfloat::rf32x2;
-use rfloat::simd::SseArth;
-use frounding::RoundingState;
+use rfloat::rf32x4;
+use rfloat::rf64x2;
 
 fn main() {
-    let mut rs = RoundingState::new();
-    let mut a: rf32x2 = rf32x2 { v0: 1.0f32, v1: 1.0f32 };
-    let b: rf32x2 = rf32x2 { v0: 3.0f32, v1: 3.0f32 };
-    rs.upward();
-    println!("rounding sse {}, fpu {}",rs.current_rounding().0, rs.current_rounding().1);
-    println!("1,1/3,3 = {}", a / b);
-    println!("1,1+3,3 = {}", a + b);
-    println!("1,1x3,3 = {}", a * b);
-    println!("1,1-3,3 = {}", a - b);
-    println!("sqrt(3,3) = {}", b.sqrtps());
-    println!("max(1,1;3,3) = {}", a.maxps(b));
-    println!("min(1,1;3,3) = {}", a.minps(b));
-    rs.downward();
-    println!("rounding sse {}, fpu {}",rs.current_rounding().0, rs.current_rounding().1);
-    println!("1,1/3,3 = {}", a / b);
-    println!("1,1+3,3 = {}", a + b);
-    println!("1,1x3,3 = {}", a * b);
-    println!("1,1-3,3 = {}", a - b);
-    println!("sqrt(3,3) = {}", b.sqrtps());
-    println!("max(1,1;3,3) = {}", a.maxps(b));
-    println!("min(1,1;3,3) = {}", a.minps(b));
-    a += b;
-    println!("{}", a);
-    a -= 3f32;
-    println!("{}", a);
-    rs.upward();
-    a /= 3f32;
-    println!("{}", a);
-    a *= 3f32;
-    a /= 3f32;
-    println!("{}", a);
+	let mut rs = frounding::RoundingState::new();
+	rs.upward();
+    let mut b = rf32x2!(1f32);
+    let mut c = rf32x4!(1f32);
+    let mut d = rf64x2!(1f64);
+    d /= 3.0;
+    b /= 3f32;
+    c /= 6f32;
+    println!("{} / {} / {}", b, c, d);
 }
