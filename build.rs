9156734a61789bed8dffb3aaadeddcea126ca4e6
GHIAT Houssem <ghiat.houssem@gmail.com>
2016-05-30 17:06:26 +0200

    add conditional compilation based on the cpu capabilities. (presence of sse, sse2 ...).

--- build.rs
+++ build.rs
@@ -1,6 +1,18 @@
 
 extern crate gcc;
+extern crate cupid;
 
-fn main(){
-	gcc::compile_library("libsimd.a", &["csrc/sse.c"]);
+fn main() {
+	let mut cfiles: Vec<&str> = Vec::new();
+    if let Some(cpu_info) = cupid::master() {
+        if cpu_info.sse() {
+        	cfiles.push("csrc/sse.c");
+        	println!("cargo:rustc-cfg=sse");
+        }
+        if cpu_info.sse2() {
+        	cfiles.push("csrc/sse2.c");
+        	println!("cargo:rustc-cfg=ss2");
+        }
+    }
+    gcc::compile_library("libsimd.a", &cfiles);
 }
